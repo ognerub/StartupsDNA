64b946742e9fd3625d82b2cd37bb2124fe835579
Alexander Ulianovskiy <131038990+ognerub@users.noreply.github.com>
2025-09-12 11:39:27 +0300

    init commit

---
+++ StartupsDNA/StartupsDNAApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartupsDNAApp.swift
+//  StartupsDNA
+//
+//  Created by Alexander Ognerubov on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct StartupsDNAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StartupsDNA/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartupsDNA
+//
+//  Created by Alexander Ognerubov on 12.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
